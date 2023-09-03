601e80f5f2f60cc5d5d6b3c23e6d0d429e567bab
Lim Zhong Zhi <jzhongzhi@gmail.com>
2023-09-03 22:18:49 +0800

    Create Document.js
    Created Document.js

---
+++ Backend/models/Document.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+    const Document = sequelize.define("Document", {
+        documentId: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        document: {
+            type: DataTypes.BLOB,
+            allowNull: false,
+        },
+        timestamp: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    })
+
+    Document.prototype.changeDescription = function(newDescription) {
+        this.description = newDescription;
+    }
+
+    Document.prototype.changeTitle = function(newTitle) {
+        this.title = newTitle;
+    }
+
+    Document.associate = function(models) {
+        Document.belongsTo(models.User, { foreignKey: 'userId' });
+        Document.belongsTo(models.Transaction, { foreignKey: 'transactionId' });
+    }
+
+    return Document;
+}
