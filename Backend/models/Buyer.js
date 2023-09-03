eebca2a65f0346f8254771d996c622825f4cbba6
Kathleen <e0774069@u.nus.edu>
2023-09-04 02:00:32 +0800

    done

--- Backend/models/Buyer.js
+++ Backend/models/Buyer.js
@@ -1,10 +1,54 @@
-const Users = require('./Users.js'); // Import the parent Users model
+ const Users = require('./Users.js'); // Import the parent Users model
 
 module.exports = (sequelize, DataTypes) => {
   sequelize.models = {};
 
   const Buyer = sequelize.define("Buyer", {
-
+      buyerId: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      token: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      countryOfOrigin: {
+        type: DataTypes.ENUM('SINGAPORE', 'MALAYSIA', 'INDONESIA'),
+        allowNull: true,
+      },
+      dateOfBirth: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+      },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: true,
+      },
+      rating: {
+        type: DataTypes.DOUBLE,
+        allowNull: true,
+        defaultValue: 0,
+      },
   });
 
   return Buyer;
--- Backend/routes/Users.js
+++ Backend/routes/Users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {Users} = require("../models")
+const {Users, Lawyer} = require("../models")
 
 router.get("/", async (req, res)=>{
     const listOfUsers = await Users.findAll();
