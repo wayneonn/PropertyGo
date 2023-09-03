f6a348e7c6c00a4938b7d073b2013e1f2c8263b3
Lim Zhong Zhi <jzhongzhi@gmail.com>
2023-09-03 16:02:17 +0800

    Create Property.js
    This is the Property JS model.

---
+++ Backend/models/Property.js
@@ -0,0 +1,86 @@
+module.exports = (sequelize, DataTypes) => {
+    const Property = sequelize.define("Property", {
+        propertyListingId: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        },
+        images: {
+            type: DataTypes.BLOB,
+            allowNull: true,
+        },
+        postedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DOUBLE,
+            allowNull: false,
+        },
+        offeredPrice: {
+            type: DataTypes.DOUBLE,
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        bed: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        bathroom: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        size: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        top: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        tenure: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        propertyType: {
+            type: DataTypes.ENUM('type1', 'type2', 'type3'), // replace with your types
+            allowNull: false,
+        },
+        propertyStatus: {
+            type: DataTypes.ENUM('status1', 'status2', 'status3'), // replace with your statuses
+            allowNull: false,
+        },
+    });
+
+    Property.prototype.changePrice = function(newPrice) {
+        this.price = newPrice;
+        return this.save();
+    };
+
+    Property.prototype.changeDescription = function(newDescription) {
+        this.description = newDescription;
+        return this.save();
+    };
+
+    Property.prototype.changeImages = function(newImages) {
+        this.images = newImages;
+        return this.save();
+    };
+
+    Property.prototype.changeTitle = function(newTitle) {
+        this.title = newTitle;
+        return this.save();
+    };
+
+    return Property;
+}
