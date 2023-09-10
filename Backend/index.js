bdd932a39190dd0a3b21e408965582857ce58177
Wayne <e0726777@u.nus.edu>
2023-09-10 22:53:50 +0800

    Modified attributes and added user test data

--- Backend/index.js
+++ Backend/index.js
@@ -3,19 +3,38 @@ const app = express()
 
 app.use(express.json())
 
-const db = require('./models')
+const db = require('./models');
+const userTestData = require('./test_data/userTestData');
 
 // Routers
 const postRouter = require('./routes/User')
 app.use("/user", postRouter)
 
-db.sequelize.sync()
-    .then(() => {
-        app.listen(3000, () => {
-            console.log('Server running on port 3000');
-        });
-    })
-    .catch((error) => {
-        console.error('Sequelize sync error:', error);
-    });
 
+db.sequelize.sync()
+    .then(async () => {
+        const existingRecordsCount = await db.User.count();
+
+        if (existingRecordsCount === 0) {
+            try {
+                for (const data of userTestData) {
+                    await db.User.create(data);
+                }
+                console.log('Test data inserted successfully.');
+            } catch (error) {
+                console.error('Error inserting test data:', error);
+            }
+        } else {
+            console.log('Test data already exists in the database.');
+        }
+
+        db.sequelize.sync()
+            .then(() => {
+                app.listen(3000, () => {
+                    console.log('Server running on port 3000');
+                });
+            })
+            .catch((error) => {
+                console.error('Sequelize sync error:', error);
+            });
+    })
