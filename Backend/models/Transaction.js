89913a3cb02a10380738612a8b84e6f10aae51f9
Wayne <e0726777@u.nus.edu>
2023-09-13 05:56:05 +0800

    Transaction- Removed extra invoice rs and added request rs

--- Backend/models/Transaction.js
+++ Backend/models/Transaction.js
@@ -19,14 +19,15 @@ module.exports = (sequelize, DataTypes) => {
     })
 
     Transaction.associate = function (models) {
-        Transaction.hasMany(models.Document, { foreignKey: 'transactionId' });
-        Transaction.belongsTo(models.User, { as: 'buyer', foreignKey: 'userId' });
-        Transaction.belongsTo(models.Property, { foreignKey: 'propertyId' });
-        Transaction.belongsTo(models.Invoice, { foreignKey: 'invoiceId' });
-        Transaction.belongsTo(models.User, { as: 'seller', foreignKey: 'userId' });
+        Transaction.hasMany(models.Document, { foreignKey: 'transactionId', allowNull: false,});
+        Transaction.belongsTo(models.Request, { as: 'request', foreignKey: 'requestId', allowNull: false, });
+        Transaction.belongsTo(models.User, { as: 'buyer', foreignKey: 'userId', allowNull: false, });
+        Transaction.belongsTo(models.Property, { foreignKey: 'propertyId' , as: 'propertyListing'});
+        Transaction.belongsTo(models.User, { as: 'seller', foreignKey: 'userId', allowNull: false, });
         Transaction.belongsTo(models.Invoice, {
             foreignKey: 'invoiceId', // This should match the foreign key in the Invoice model
             onDelete: 'CASCADE', // If a transaction is deleted, delete the associated invoice
+            allowNull: false,
         });
     }
 
