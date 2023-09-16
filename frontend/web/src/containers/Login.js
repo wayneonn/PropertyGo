eadb43ab2de61fda680ee501cadd976894caee56
wjahoward <howardwang42@gmail.com>
2023-09-16 12:47:36 +0800

    some cleaning for Login.js

--- frontend/web/src/containers/Login.js
+++ frontend/web/src/containers/Login.js
@@ -1,15 +1,19 @@
 // components
 import React, { useState } from "react";
-import { 
+import {
   Card,
-  Row, 
-  Col, 
+  Row,
+  Col,
   Button,
   Form,
   InputGroup,
-  Toast } from "react-bootstrap";
+  Toast
+} from "react-bootstrap";
 import { BiRightArrowAlt } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
+
+// css
+import "./styles/Login.css";
 
 // utils
 import API from "../services/API";
@@ -37,11 +41,11 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     e.stopPropagation();
-    
+
     setValidated(true);
 
     const form = e.currentTarget;
-    
+
     // Check if both userName and password fields have values
     if (form.checkValidity() === true && userName && password) {
       try {
@@ -53,147 +57,94 @@ const Login = () => {
         if (response.status === 200) {
           // TODO: Add localStorage
           navigate("/login");
-        } 
+        }
 
       } catch (error) {
         setShow(true);
       }
-  }
+    }
   };
 
   return (
-    <div
-      style={{
-        backgroundColor: "#F5F6F7",
-        position: "fixed",
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div>
-        <div
-          style={{
-            position: "fixed",
-            top: "10%", // Adjust this value to control the vertical position
-            left: "55%", // Center horizontally
-            transform: "translateX(-50%)", // Center horizontally
-            }}
-          >
-          <Row>
-              <Col xs={6}>
-                <Toast bg="warning" onClose={() => setShow(false)} show={show} delay={4000} autohide >
-                  <Toast.Header>
-                    <strong className="me-auto">Login Unsuccessful</strong>
-                  </Toast.Header>
-                  <Toast.Body>Please key in the valid credentials!</Toast.Body>
-                </Toast>
-              </Col>
-          </Row>
-        </div>
-        <Card>
-          <Row nogutters="true" style={{ display: "flex" }}>
-            <Col>
+    <div className="loginContainer">
+      <div className="loginToast">
+        <Row>
+          <Col xs={6}>
+            <Toast bg="warning" onClose={() => setShow(false)} show={show} delay={4000} autohide >
+              <Toast.Header>
+                <strong className="me-auto">Login Unsuccessful</strong>
+              </Toast.Header>
+              <Toast.Body>Please key in the valid credentials!</Toast.Body>
+            </Toast>
+          </Col>
+        </Row>
+      </div>
+      <Card>
+        <Row nogutters="true" className="loginCardRow">
+          <Col>
+            <Card.Img
+              src="login.jpeg"
+              className="loginCardImage"
+            />
+          </Col>
+          <Col className="loginRightSideCard">
+            <Card.Body className="loginRightSideCardContent">
               <Card.Img
-                src="login.jpeg"
-                style={{ width: "508px", height: "432px" }}
+                src="Logo.png"
+                className="loginPropertyGoIcon"
               />
-            </Col>
-            <Col
-              style={{
-                background: "#FFFFFF",
-                width: "508px",
-                height: "432px",
-                display: "flex",
-                justifyContent: "center", // Center horizontally
-              }}
-            >
-              <Card.Body
-                style={{
-                  padding: 0,
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <Card.Img
-                  src="Logo.png"
-                  style={{
-                    width: "100.78px",
-                    height: "100.78px",
-                    marginTop: "1em",
-                  }}
-                  alt="..."
-                />
-                <div style={{ padding: "0.7em" }}></div>
-                <Card.Title style={{ fontWeight: "bold", fontSize: "24px" }}>
-                  Login to your account
-                </Card.Title>
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                  <div>
-                    <Form.Label htmlFor="inputUsername">Username</Form.Label>
-                    <InputGroup hasValidation>
-                      <Form.Control
-                        required
-                        id="inputUsername"
-                        placeholder="Username"
-                        style={{
-                          width: "30em",
-                          height: "2.5em",
-                          borderRadius: "0.5em",
-                        }}
-                        onChange={handleUserNameChange}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        Please key in a username.
-                      </Form.Control.Feedback>
-                    </InputGroup>
-                  </div>
-                  <div style={{marginTop: '0.3em'}}></div>
-                  <div>
-                    <Form.Label htmlFor="inputPassword">Password</Form.Label>
+              <div className="loginSpacesBetweenInputs"></div>
+              <Card.Title className="loginTitle">
+                Login to your account
+              </Card.Title>
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <div>
+                  <Form.Label htmlFor="inputUsername">Username</Form.Label>
+                  <InputGroup hasValidation>
                     <Form.Control
                       required
-                      type="password"
-                      id="inputPassword"
-                      placeholder="Password"
-                      style={{
-                        width: "30em",
-                        height: "2.5em",
-                        borderRadius: "0.5em",
-                      }}
-                      onChange={handlePasswordChange}
+                      id="inputUsername"
+                      placeholder="Username"
+                      className="loginUsernameInput"
+                      onChange={handleUserNameChange}
                     />
                     <Form.Control.Feedback type="invalid">
-                      Please key in a password.
+                      Please key in a username.
                     </Form.Control.Feedback>
-                  </div>
-                  <div style={{marginTop: '1em'}}></div>
-                  <div className="d-grid gap-2">
-                    <Button
-                      variant="warning"
-                      size="md"
-                      type="submit"
-                      style={{
-                        borderRadius: "160px",
-                        backgroundColor: "#FDE933",
-                        borderColor: "#FDE933",
-                      }}
-                    >
-                      <span style={{ marginRight: "0.3em" }}>Login</span>
-                      <BiRightArrowAlt />
-                    </Button>
-                  </div>
-                </Form>
-              </Card.Body>
-            </Col>
-          </Row>
-        </Card>
-      </div>
+                  </InputGroup>
+                </div>
+                <div className="loginSpacesBetweenInputs"></div>
+                <div>
+                  <Form.Label htmlFor="inputPassword">Password</Form.Label>
+                  <Form.Control
+                    required
+                    type="password"
+                    id="inputPassword"
+                    placeholder="Password"
+                    className="loginPasswordInput"
+                    onChange={handlePasswordChange}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please key in a password.
+                  </Form.Control.Feedback>
+                </div>
+                <div className="loginSpaceBetweeonPasswordInputButton"></div>
+                <div className="d-grid gap-2">
+                  <Button
+                    variant="warning"
+                    size="md"
+                    type="submit"
+                    className="loginButton"
+                  >
+                    <span className="loginTextButton">Login</span>
+                    <BiRightArrowAlt />
+                  </Button>
+                </div>
+              </Form>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
     </div>
   );
 };
