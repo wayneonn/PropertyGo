223f634445f502864f23d822c15b15263fa8e7dd
Wayne <e0726777@u.nus.edu>
2023-09-16 23:56:19 +0800

    Updated UI of LoginScreen

--- frontend/mobile/navigations/SideNavigator.js
+++ frontend/mobile/navigations/SideNavigator.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeStackGroup from './HomeStackGroup';
-
+import Logout from '../screens/login/LoginPortal';
 
 const Drawer = createDrawerNavigator();
 
 const SideBar = () => {
-
     return (
         <Drawer.Navigator screenOptions={{ headerShown: false }}>
             <Drawer.Screen name="Home Stack Group" component={HomeStackGroup} />
-            {/* <Drawer.Screen name="Profile" component={Profile} /> */}
-            {/* Add more screens as needed */}
+            {/* <Drawer.Screen name="Explore Services" component={ExploreServices} />
+            <Drawer.Screen name="User Profile" component={UserProfile} />
+            <Drawer.Screen name="Listings" component={Listings} />
+            <Drawer.Screen name="Appointments" component={Appointments} />
+            <Drawer.Screen name="Transactions" component={Transactions} />
+            <Drawer.Screen name="Documents" component={Documents} />
+            <Drawer.Screen name="Mortgage Calculator" component={MortgageCalculator} />
+            <Drawer.Screen name="Work with Us" component={WorkWithUs} />
+            <Drawer.Screen name="Help & Support" component={HelpAndSupport} />
+            <Drawer.Screen name="Contact Us" component={ContactUs} /> */}
+            <Drawer.Screen name="Logout" component={Logout} />
         </Drawer.Navigator>
     );
 };
+
 export default SideBar;
--- frontend/mobile/screens/login/LoginScreen.js
+++ frontend/mobile/screens/login/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { loginUser } from '../../utils/api';
 
 function LoginScreen({ navigation }) {
@@ -21,35 +21,96 @@ function LoginScreen({ navigation }) {
       showMessage('Login successful');
       setTimeout(() => {
         navigation.navigate('Side Navigator', { user: data });
-      }, 2000);
+      }, 500);
     } else {
       showMessage(message);
     }
   };
 
   return (
-    <View style={{ padding: 16 }}>
+    <View style={styles.container}>
+      <View style={styles.iconContainer}>
+        {/* Icon at the top center */}
+        <Image
+          source={require('../../assets/PropertyGo-HighRes-Logo.png')} // Replace with the actual path to your image
+          style={styles.iconImage}
+        />
+      </View>
       <TextInput
-        style={{ padding: 16, marginBottom: 8, borderWidth: 1, borderRadius: 8 }}
+        style={styles.input}
         placeholder="Username"
+        placeholderTextColor="black"
         value={userName}
         onChangeText={setUserName}
       />
       <TextInput
-        style={{ padding: 16, marginBottom: 8, borderWidth: 1, borderRadius: 8 }}
+        style={styles.input}
         placeholder="Password"
+        placeholderTextColor="black"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title="Login" onPress={handleLogin} />
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
       {message && (
-        <View style={{ position: 'absolute', top: 50, left: 50, backgroundColor: 'lightgray', padding: 10, borderRadius: 5 }}>
-          <Text>{message}</Text>
+        <View style={styles.messageContainer}>
+          <Text style={styles.messageText}>{message}</Text>
         </View>
       )}
     </View>
   );
 }
+
+const styles = {
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  iconContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  input: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingLeft: 10,
+    color: 'black',
+  },
+  loginButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
+    borderRadius: 25,
+    backgroundColor: '#1E90FF',
+    marginVertical: 10,
+  },
+  loginButtonText: {
+    fontSize: 18,
+    color: 'white',
+  },
+  messageContainer: {
+    position: 'absolute',
+    top: 150,
+    alignSelf: 'center',
+    backgroundColor: 'rgba(144, 238, 144, 0.8)',
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+    marginBottom: 20,
+  },
+  messageText: {
+    fontSize: 16,
+    color: 'black',
+  },
+  iconImage: {
+    width: 60,
+    height: 60,
+  },
+};
 
 export default LoginScreen;
