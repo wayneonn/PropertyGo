c0e48e673f638aa4d98ce9afa2a33e4ba9fb141b
wjahoward <howardwang42@gmail.com>
2023-09-19 21:25:06 +0800

    update

--- frontend/web/src/components/Common/NotFound.js
+++ frontend/web/src/components/Common/NotFound.js
@@ -9,34 +9,40 @@ const NotFound = () => {
   const isLoggedIn = localStorage.getItem("loggedInAdmin");
 
   return (
-    <div className="not-found">
-      {isLoggedIn && (
-        <div>
-          <SideBar />
-          <TopBar />
-        </div>
-      )}
-
-      {isLoggedIn ? (
-        <div className="not-found"
-          style={{
-            textAlign: "center",
-          }}
-        >
-          <h3
-            style={{
-              fontWeight: "400",
-              font: "Montserrat",
-            }}
-          >
-            404 Page Not Found
-          </h3>
-          <span>The page that you are looking for does not exist.</span>
-        </div>
-      ) : (
-        <LogoutNotFound />
-      )}
-    </div>
+    <>
+      <div>
+        {isLoggedIn && (
+          <div>
+            <SideBar />
+            <TopBar />
+          </div>
+        )}
+        ;
+      </div>
+      <div>
+        {isLoggedIn ? (
+          <div className="not-found">
+            <div
+              style={{
+                textAlign: "center",
+              }}
+            >
+              <h3
+                style={{
+                  fontWeight: "400",
+                  font: "Montserrat",
+                }}
+              >
+                404 Page Not Found
+              </h3>
+              <span>The page that you are looking for does not exist.</span>
+            </div>
+          </div>
+        ) : (
+          <LogoutNotFound />
+        )}
+      </div>
+    </>
   );
 };
 
