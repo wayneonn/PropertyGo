2815e82f2813a30d3a7c0d3b93b6f25085a42daa
Kathleen <e0774069@u.nus.edu>
2023-09-20 15:34:32 +0800

    validation

--- Backend/controllers/admin/adminController.js
+++ Backend/controllers/admin/adminController.js
@@ -2,77 +2,94 @@ const { Admin } = require("../../models");
 
 // helper function
 const getAdmin = (userName) => {
-    return Admin.findOne({ where: { userName } });
+  return Admin.findOne({ where: { userName } });
 };
 
 const getSingleAdmin = async (req, res) => {
-    try {
-        const { id: adminId } = req.params;
+  try {
+    const { id: adminId } = req.params;
 
-        const admin = await Admin.findByPk(adminId);
+    const admin = await Admin.findByPk(adminId);
 
-        if (!admin) {
-            return res.status(404).json({ message: "Admin not found" });
-        }
-
-        res.status(200).json(admin);
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+    if (!admin) {
+      return res.status(404).json({ message: "Admin not found" });
     }
+
+    res.status(200).json(admin);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 const updateAdminUsername = async (req, res) => {
-    try {
-        const { oldUserName, updatedUserName } = req.body;
+  try {
+    const { oldUserName, updatedUserName } = req.body;
 
-        const admin = await getAdmin(updatedUserName);
+    const admin = await getAdmin(updatedUserName);
 
-        if (admin) {
-            return res.status(409).json({ message: "Admin UserName found. Change to another userName" });
-        }
-        const currentAdmin = await getAdmin(oldUserName);
-
-        currentAdmin.userName = updatedUserName;
-
-        await currentAdmin.save();
-
-        res.status(200).json({ message: "You have updated your userName successfully!" });
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+    if (oldUserName === updatedUserName) {
+      return res.status(200).json({ message: "unchanged" });
     }
+
+    if (admin) {
+      return res
+        .status(409)
+        .json({ message: "Admin UserName found. Change to another userName" });
+    }
+    const currentAdmin = await getAdmin(oldUserName);
+
+    currentAdmin.userName = updatedUserName;
+
+    await currentAdmin.save();
+
+    res
+      .status(200)
+      .json({ message: "You have updated your userName successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 const updateAdminPassword = async (req, res) => {
-    try {
-        const { userName, oldPassword, newPassword, newConfirmedPassword } = req.body;
+  try {
+    const { userName, oldPassword, newPassword, newConfirmedPassword } =
+      req.body;
 
-        const admin = await getAdmin(userName);
+    const admin = await getAdmin(userName);
 
-        if (!admin) {
-            return res.status(404).json({ message: "Admin not found" });
-        }
-
-        const currentPassword = admin.password;
-
-        if (currentPassword !== oldPassword) {
-            return res.status(401).json({ message: "Password is incorrect"});
-        }
-
-        if (newPassword !== newConfirmedPassword) {
-            return res.status(400).json({ message: "New password and confirmed passwords are different"});
-        }
-
-        admin.password = newPassword;
-        await admin.save();
-
-        res.status(200).json({ message: "You have updated your password successfully!" });
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+    if (!admin) {
+      return res.status(404).json({ message: "Admin not found" });
     }
+
+    const currentPassword = admin.password;
+
+    // if (currentPassword === oldPassword) {
+    //   return res.status(200).json({ message: "unchanged" });
+    // }
+
+    if (currentPassword !== oldPassword) {
+      return res.status(401).json({ message: "Password is incorrect" });
+    }
+
+    if (newPassword !== newConfirmedPassword) {
+      return res.status(400).json({
+        message: "New password and confirmed passwords are different",
+      });
+    }
+
+    admin.password = newPassword;
+    await admin.save();
+
+    res
+      .status(200)
+      .json({ message: "You have updated your password successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 module.exports = {
-    getSingleAdmin,
-    updateAdminUsername,
-    updateAdminPassword
+  getSingleAdmin,
+  updateAdminUsername,
+  updateAdminPassword,
 };
