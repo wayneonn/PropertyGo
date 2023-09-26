a98dafcd025662742c5eee1ef81a782d327c3562
Wayne <e0726777@u.nus.edu>
2023-09-27 07:23:12 +0800

    Updated UI for Sell.js

--- frontend/mobile/screens/userBottomNavBar/Sell.js
+++ frontend/mobile/screens/userBottomNavBar/Sell.js
@@ -9,29 +9,38 @@ import {
   Image,
   TouchableOpacity,
   Alert,
+  Modal,
 } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import * as ImagePicker from 'expo-image-picker';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { createProperty } from '../../utils/api'; // Import the createProperty function
+import { createProperty } from '../../utils/api';
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons from the correct library
 
+const propertyTypes = [
+  { label: 'Select Property Type', value: '' },
+  { label: 'Resale', value: 'Resale' },
+  { label: 'New Launch', value: 'New Launch' },
+];
 
 export default function PropertyListing() {
   const [property, setProperty] = useState({
     title: 'Sample Title',
     description:
-      'Sample Description (You can add a longer description here.)', // Adjust the initial description
-    price: '100000',
-    offeredPrice: '90000',
+      'Sample Description (You can add a longer description here.)',
+    price: '100000', // Add a dollar symbol to the price
+    offeredPrice: '90000', // Add a dollar symbol to the offered price
     bed: '2',
     bathroom: '2',
     size: '1200',
     tenure: 1,
-    propertyType: 'RESALE',
+    propertyType: '',
     propertyStatus: 'ACTIVE',
     userId: 1,
   });
 
   const [images, setImages] = useState([]);
+  const [propertyTypeVisible, setPropertyTypeVisible] = useState(false);
 
   const handleChoosePhoto = async () => {
     const permissionResult =
@@ -112,91 +121,182 @@ export default function PropertyListing() {
       return;
     }
 
+    // Convert property type to uppercase
+    let propertyTypeUpperCase = property.propertyType.toUpperCase();
+
+    // Check if propertyType is 'New Launch' and convert it to 'NEW_LAUNCH'
+    if (propertyTypeUpperCase === 'NEW LAUNCH') {
+      propertyTypeUpperCase = 'NEW_LAUNCH';
+    }
+
     try {
-      // Call the createProperty function to create the property listing
-      const { success, data, message } = await createProperty(property, images);
+      const { success, data, message } = await createProperty(
+        {
+          ...property,
+          price: property.price.replace(/\$/g, ''), // Remove dollar symbol
+          offeredPrice: property.offeredPrice.replace(/\$/g, ''), // Remove dollar symbol
+          propertyType: propertyTypeUpperCase, // Set property type to uppercase
+        },
+        images
+      );
 
       if (success) {
-        // Property created successfully
-        Alert.alert('Property Created', 'The property listing has been created successfully.');
+        Alert.alert(
+          'Property Created',
+          'The property listing has been created successfully.'
+        );
       } else {
-        // Property creation failed, show an error message
         Alert.alert('Error', `Failed to create property: ${message}`);
       }
     } catch (error) {
       console.log('Error uploading property:', error);
-      Alert.alert('Error', 'An error occurred while creating the property listing.');
+      Alert.alert(
+        'Error',
+        'An error occurred while creating the property listing.'
+      );
     }
   };
 
+
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.imageRow}>
-        <TouchableOpacity onPress={handleChoosePhoto} style={styles.imagePicker}>
-          <Icon name="camera" size={40} color="#aaa" />
-        </TouchableOpacity>
-
-        {images.map((image, index) => (
-          <TouchableOpacity
-            key={index}
-            onPress={() => handleImagePress(index)}
-            style={styles.imageContainer}
-          >
-            <Image
-              source={{ uri: image.uri }}
-              style={styles.image}
-            />
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.imageRow}>
+          <TouchableOpacity onPress={handleChoosePhoto} style={styles.imagePicker}>
+            <Icon name="camera" size={40} color="#aaa" />
           </TouchableOpacity>
-        ))}
-      </View>
 
-      <TextInput
-        placeholder="Title"
-        value={property.title}
-        onChangeText={(text) => setProperty({ ...property, title: text })}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Property Type (RESALE/NEW_LAUNCH)"
-        value={property.propertyType}
-        onChangeText={(text) => setProperty({ ...property, propertyType: text })}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Bed"
-        value={property.bed}
-        onChangeText={(text) => setProperty({ ...property, bed: text })}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Bathroom"
-        value={property.bathroom}
-        onChangeText={(text) => setProperty({ ...property, bathroom: text })}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Size"
-        value={property.size}
-        onChangeText={(text) => setProperty({ ...property, size: text })}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Description"
-        value={property.description}
-        onChangeText={(text) => setProperty({ ...property, description: text })}
-        style={[styles.input, styles.largeTextInput]} // Larger TextInput
-        multiline={true}
-        numberOfLines={4}
-      />
-      <TextInput
-        placeholder="Price"
-        value={property.price}
-        onChangeText={(text) => setProperty({ ...property, price: text })}
-        style={styles.input}
-      />
+          {images.map((image, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => handleImagePress(index)}
+              style={styles.imageContainer}
+            >
+              <Image source={{ uri: image.uri }} style={styles.image} />
+            </TouchableOpacity>
+          ))}
+        </View>
 
-      <Button title="Submit" onPress={handleSubmit} />
-    </ScrollView>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput
+            placeholder="Title"
+            value={property.title}
+            onChangeText={(text) => setProperty({ ...property, title: text })}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Bed</Text>
+          <TextInput
+            placeholder="Bed"
+            value={property.bed}
+            onChangeText={(text) => setProperty({ ...property, bed: text })}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Bathroom</Text>
+          <TextInput
+            placeholder="Bathroom"
+            value={property.bathroom}
+            onChangeText={(text) =>
+              setProperty({ ...property, bathroom: text })
+            }
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Size</Text>
+          <TextInput
+            placeholder="Size"
+            value={property.size}
+            onChangeText={(text) => setProperty({ ...property, size: text })}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput
+            placeholder="Description"
+            value={property.description}
+            onChangeText={(text) =>
+              setProperty({ ...property, description: text })
+            }
+            style={[styles.input, styles.largeTextInput]}
+            multiline={true}
+            numberOfLines={4}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput
+            placeholder="$ Price"
+            value={property.price}
+            onChangeText={(text) => setProperty({ ...property, price: text })}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Property Type</Text>
+          <TouchableOpacity
+            style={styles.propertyTypePickerButton}
+            onPress={() => setPropertyTypeVisible(true)}
+          >
+            <Text style={styles.propertyTypePickerText}>
+              {property.propertyType
+                ? property.propertyType.charAt(0).toUpperCase() +
+                property.propertyType.slice(1)
+                : 'Select Property Type'}
+            </Text>
+            <Icon name="caret-down" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+
+        <Modal
+          transparent={true}
+          animationType="slide"
+          visible={propertyTypeVisible}
+          onRequestClose={() => setPropertyTypeVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <Picker
+              selectedValue={property.propertyType}
+              onValueChange={(value) =>
+                setProperty({ ...property, propertyType: value })
+              }
+              style={styles.picker}
+            >
+              {propertyTypes.map((type, index) => (
+                <Picker.Item
+                  key={index}
+                  label={type.label}
+                  value={type.value}
+                />
+              ))}
+            </Picker>
+            <View style={styles.okButtonContainer}>
+              <Button
+                title="OK"
+                onPress={() => setPropertyTypeVisible(false)}
+              />
+            </View>
+          </View>
+        </Modal>
+      </ScrollView>
+
+      <TouchableOpacity style={styles.saveChangesButton} onPress={handleSubmit}>
+        <Ionicons name="save-outline" size={18} color="white" />
+        <Text style={styles.saveChangesButtonText}>Save Changes</Text>
+      </TouchableOpacity>
+
+    </View>
   );
 }
 
@@ -205,15 +305,26 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  scrollView: {
+    flex: 1,
+    marginBottom: 10, // Adjust this margin to avoid overlap with the navigation bar
+  },
+  inputContainer: {
+    marginBottom: 10,
+  },
+  label: {
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
   input: {
     borderColor: 'gray',
     borderWidth: 1,
-    marginBottom: 10, // Increased margin
+    marginBottom: 10,
     padding: 8,
     height: 40,
   },
   largeTextInput: {
-    height: 120, // Increased height for larger input
+    height: 120,
   },
   imageRow: {
     flexDirection: 'row',
@@ -236,4 +347,46 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
+  propertyTypePickerButton: {
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    borderColor: 'gray',
+    fontSize: 14,
+    padding: 8,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  propertyTypePickerText: {
+    fontSize: 14,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingBottom: 20, // Add padding to make the button visible
+  },
+  picker: {
+    backgroundColor: 'white',
+  },
+  okButtonContainer: {
+    backgroundColor: 'white',
+  },
+  saveChangesButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'center', // Center horizontally
+    flexDirection: 'row',
+    justifyContent: 'center', // Center vertically
+    width: '60%',
+    marginLeft: 70,
+},
+saveChangesButtonText: {
+    color: 'white',
+    marginLeft: 10,
+}, 
 });
