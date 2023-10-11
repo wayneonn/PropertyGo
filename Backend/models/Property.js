a5102583c0c64554acde4c0c497dc673cbb73bf5
Kathleen <e0774069@u.nus.edu>
2023-10-11 13:30:08 +0800

    property

--- Backend/models/Property.js
+++ Backend/models/Property.js
@@ -1,102 +1,107 @@
 module.exports = (sequelize, DataTypes) => {
-  const Property = sequelize.define("Property", {
-    propertyListingId: {
-      type: DataTypes.BIGINT,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
+  const Property = sequelize.define(
+    "Property",
+    {
+      propertyListingId: {
+        type: DataTypes.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      images: {
+        type: DataTypes.BLOB,
+        allowNull: true,
+      },
+      postedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      offeredPrice: {
+        type: DataTypes.DOUBLE,
+        allowNull: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      bed: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      bathroom: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      size: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      top: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      tenure: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      propertyType: {
+        type: DataTypes.ENUM("RESALE", "NEW_LAUNCH"),
+        allowNull: false,
+      },
+      propertyStatus: {
+        type: DataTypes.ENUM("ACTIVE", "ON_HOLD", "COMPLETED"),
+        allowNull: false,
+      },
+      postalCode: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      unitNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      area: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      region: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      boostListingStartDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      boostListingEndDate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      longitude: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      latitude: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-    images: {
-      type: DataTypes.BLOB,
-      allowNull: true,
-    },
-    postedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    offeredPrice: {
-      type: DataTypes.DOUBLE,
-      allowNull: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    bed: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    bathroom: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    size: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    top: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    tenure: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    propertyType: {
-      type: DataTypes.ENUM('RESALE', 'NEW_LAUNCH'),
-      allowNull: false,
-    },
-    propertyStatus: {
-      type: DataTypes.ENUM('ACTIVE', 'ON_HOLD', 'COMPLETED'),
-      allowNull: false,
-    },
-    postalCode: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    unitNumber: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    area: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    region: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    boostListingStartDate: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    boostListingEndDate: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    longitude: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    latitude: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  }, {
-    freezeTableName: true
-  });
+    {
+      freezeTableName: true,
+    }
+  );
+
   Property.belongsTo(models.User, {
     foreignKey: {
       name: "userId",
