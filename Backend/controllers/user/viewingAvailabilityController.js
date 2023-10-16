10470f9fc3b3a6a5cdccc3c07b6f5e615f8091e2
Wayne <e0726777@u.nus.edu>
2023-10-16 11:28:55 +0800

    Added backend to search ViewAvailability by date and PropertyId

--- Backend/controllers/user/viewingAvailabilityController.js
+++ Backend/controllers/user/viewingAvailabilityController.js
@@ -67,9 +67,33 @@ async function deleteViewingAvailability(req, res) {
     }
 }
 
+async function getViewingAvailabilityByDateAndPropertyId(req, res) {
+    const { date, propertyId } = req.query;
+
+    try {
+        // Query the database to find viewing availability for the specified date and property ID
+        const availability = await ViewingAvailability.findAll({
+            where: {
+                date,
+                propertyListingId: propertyId,
+            },
+        });
+
+        if (!availability || availability.length === 0) {
+            return res.status(404).json({ error: 'Viewing availability not found for the specified date and property ID' });
+        }
+
+        res.json(availability);
+    } catch (error) {
+        console.error('Error fetching viewing availability by date and property ID:', error);
+        res.status(500).json({ error: 'Error fetching viewing availability by date and property ID' });
+    }
+}
+
 module.exports = {
     createViewingAvailability,
     getViewingAvailabilityById,
     updateViewingAvailability,
     deleteViewingAvailability,
+    getViewingAvailabilityByDateAndPropertyId,
 };
--- Backend/routes/user/viewingAvailabilityRoute.js
+++ Backend/routes/user/viewingAvailabilityRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createViewingAvailability, getViewingAvailabilityById, updateViewingAvailability, deleteViewingAvailability } = require('../../controllers/user/viewingAvailabilityController');
+const { createViewingAvailability, getViewingAvailabilityById, updateViewingAvailability, deleteViewingAvailability, getViewingAvailabilityByDateAndPropertyId } = require('../../controllers/user/viewingAvailabilityController');
 
 router.post('/', createViewingAvailability);
+router.get('/date-property', getViewingAvailabilityByDateAndPropertyId);
 router.get('/:availabilityId', getViewingAvailabilityById);
 router.put('/:availabilityId', updateViewingAvailability);
 router.delete('/:availabilityId', deleteViewingAvailability);
+
 
 module.exports = router;
