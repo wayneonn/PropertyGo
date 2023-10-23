e89af229c5b2028b6777e1be719472f41993efc1
Wayne <e0726777@u.nus.edu>
2023-10-23 15:23:13 +0800

    Added StripeServices at mobile

--- frontend/mobile/screens/purchaseProperty/PurchaseOptionFee.js
+++ frontend/mobile/screens/purchaseProperty/PurchaseOptionFee.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, useCallback, useContext } from 'react';
 import { View, Button, Alert, Linking, Text } from 'react-native';
 import { useStripe, StripeProvider } from '@stripe/stripe-react-native';
-import { paymentSheet } from "../../utils/stripeApi"; // Import your new paymentSheet function
-import { updateUserProfile, loginUser } from "../../utils/api";
-import { createTransaction } from "../../utils/transactionApi";
+import { updateUserStripeCustomerId, initializePaymentSheet, createTransactionRecord, fetchUpdatedUserDetails, handleDeepLink } from "../../services/StripeServices";
 import { AuthContext } from '../../AuthContext';
 import { useNavigation } from '@react-navigation/native';
+
 const PurchaseOptionFee = ({ route }) => {
     const { propertyListing } = route.params;
     const navigation = useNavigation();
@@ -17,68 +16,11 @@ const PurchaseOptionFee = ({ route }) => {
     const [paymentIntent, setPaymentIntent] = useState('');
     const [publishableKey, setPublishableKey] = useState('');
     const [custIdExists, setCustIdExists] = useState(false);
-    const [completedPaymentIntent, setCompletedPaymentIntent] = useState(null);
     const stripeCustomerId = user.user.stripeCustomerId;
-    const amount = 1099; // Replace with the desired amount
-    const currency = 'sgd'; // Replace with the desired currency code
+    const description = "Purchase Option Fee";
 
-    const initializePaymentSheet = async () => {
-        try {
-            // Use the paymentSheet function to fetch payment parameters
-            console.log("stripeCustomerId: ", stripeCustomerId);
-            if (stripeCustomerId !== null) {
-                setCustIdExists(true);
-            }
-
-            const { data, success, message } = await paymentSheet({
-                stripeCustomerId,
-                amount,
-                currency,
-                email: user.user.email,
-                name: user.user.name,
-                description: "Purchase Option Fee",
-                isAService: true,
-            });
-
-            console.log("Data:", data, " success: ", success);
-
-            if (!success) {
-                console.error('Error fetching payment sheet data:', message);
-                // Handle the error as needed (e.g., show an error message to the user)
-                return;
-            }
-
-            if (custIdExists === false) {
-                setStripeCustomerId(data.customer);
-            }
-            setEphemeralKey(data.ephemeralKey);
-            setPaymentIntent(data.paymentIntent);
-            setPublishableKey(data.publishableKey);
-
-
-            console.log("Data:", data, " success: ", success);
-            const { error } = await initPaymentSheet({
-                merchantDisplayName: 'PropertyGo, Pte. Ltd.',
-                customerId: data.customer,
-                customerEphemeralKeySecret: data.ephemeralKey,
-                paymentIntentClientSecret: data.paymentIntent,
-                // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
-                // methods that complete payment after a delay, like SEPA Debit and Sofort.
-                allowsDelayedPaymentMethods: true,
-                defaultBillingDetails: {
-                    name: 'Jane Doe',
-                },
-            });
-
-            if (!error) {
-                setLoading(true);
-            } else {
-                console.error('Error initializing payment sheet:', error);
-                throw error;
-            }
-        } catch (error) {
-            console.error('Error setting up payment sheet:', error);
-        }
+    const initializePayment = async () => {
+        await initializePaymentSheet(stripeCustomerId, user.user, setStripeCustomerId, setEphemeralKey, setPaymentIntent, setPublishableKey, setCustIdExists, initPaymentSheet, setLoading);
     };
 
     const openPaymentSheet = async () => {
@@ -89,9 +31,9 @@ const PurchaseOptionFee = ({ route }) => {
                 Alert.alert(`Error code: ${error.code}`, error.message);
             } else {
                 if (custIdExists == false) {
-                    updateUserStripeCustomerId(newStripeCustomerId);
+                    updateUserStripeCustomerId(newStripeCustomerId, user.user, fetchUpdatedUserDetails);
                 }
-                createTransactionRecord();
+                createTransactionRecord(propertyListing, user.user, paymentIntent);
                 Alert.alert('Success', 'Your order is confirmed!');
                 navigation.navigate('Home Page');
             }
@@ -100,81 +42,28 @@ const PurchaseOptionFee = ({ route }) => {
         }
     };
 
-    const updateUserStripeCustomerId = async (stripeCustomerId) => {
-        const formData = new FormData();
-        console.log("stripeCustomerId: ", stripeCustomerId);
-        formData.append('stripeCustomerId', stripeCustomerId);
-        formData.append('email', user.user.email);
-        formData.append('countryOfOrigin', user.user.countryOfOrigin);
-        formData.append('dateOfBirth', user.user.dateOfBirth);
-
-        const { success, data, message } = await updateUserProfile(user.user.userId, formData);
-        console.log("success: ", success, " data: ", data, " message: ", message);
-        fetchUpdatedUserDetails();
-    }
-
-    const createTransactionRecord = async () => {
-        const { data, success, message } = await createTransaction({
-            onHoldBalance: propertyListing.optionFee,
-            buyerId: user.user.userId,
-            propertyId: propertyListing.propertyListingId,
-            stripePaymentResponse: paymentIntent,
-            invoiceId: 3,
-            status: "PENDING",
-            transactionType: "OPTION_FEE",
-        });
-        // console.log("createTransactionRecord - success: ", success, " data: ", data, " message: ", message);
-    }
-
-    const fetchUpdatedUserDetails = async () => {
-        try {
-            const { success, data, message } = await loginUser(user.user.userName, user.user.password);
-
-            if (success) {
-                login(data);
-            } else {
-                Alert.alert('Error', message);
-            }
-        } catch (error) {
-            console.error('Error fetching updated user details:', error);
-        }
-    };
-
-    // Handle deep linking for Stripe
-    const handleDeepLink = useCallback(async (url) => {
-        if (url) {
-            const stripeHandled = await handleURLCallback(url);
-            if (stripeHandled) {
-                // This was a Stripe URL - you can return or add extra handling here as you see fit
-            } else {
-                // This was NOT a Stripe URL – handle as you normally would
-            }
-        }
-    }, [handleURLCallback]);
-
     useEffect(() => {
-        initializePaymentSheet();
+        initializePayment();
 
         // Handle deep linking
         const getUrlAsync = async () => {
             const initialUrl = await Linking.getInitialURL();
-            handleDeepLink(initialUrl);
+            handleDeepLink(initialUrl, handleURLCallback);
         };
 
         getUrlAsync();
 
         const deepLinkListener = Linking.addEventListener('url', (event) => {
-            handleDeepLink(event.url);
+            handleDeepLink(event.url, handleURLCallback);
         });
 
         return () => deepLinkListener.remove();
-    }, [handleDeepLink]);
+    }, [handleURLCallback]);
 
     return (
         <View>
             <StripeProvider
                 publishableKey={publishableKey}
-            //merchantIdentifier="merchant.identifier" // required for Apple Pay
             >
                 <Button
                     variant="primary"
---
+++ frontend/mobile/services/StripeServices.js
@@ -0,0 +1,126 @@
+// StripeServices.js
+import { useStripe } from '@stripe/stripe-react-native';
+import { paymentSheet } from "../utils/stripeApi";
+import { updateUserProfile, loginUser } from "../utils/api";
+import { createTransaction } from "../utils/transactionApi";
+
+export const initializePaymentSheet = async (
+    stripeCustomerId,
+    user,
+    setStripeCustomerId,
+    setEphemeralKey,
+    setPaymentIntent,
+    setPublishableKey,
+    setCustIdExists,
+    initPaymentSheet, // Pass initPaymentSheet as a parameter
+    setLoading
+  ) => {
+    try {
+      // Use the paymentSheet function to fetch payment parameters
+      console.log("stripeCustomerId: ", stripeCustomerId);
+      if (stripeCustomerId !== null) {
+        setCustIdExists(true);
+      }
+  
+      const amount = 1099; // Replace with the desired amount
+      const currency = 'sgd'; // Replace with the desired currency code
+  
+      const { data, success, message } = await paymentSheet({
+        stripeCustomerId,
+        amount,
+        currency,
+        email: user.email,
+        name: user.name,
+        description: "Purchase Option Fee",
+        isAService: true,
+      });
+  
+      console.log("Data:", data, " success: ", success);
+  
+      if (!success) {
+        console.error('Error fetching payment sheet data:', message);
+        // Handle the error as needed (e.g., show an error message to the user)
+        return;
+      }
+  
+      if (stripeCustomerId === null) {
+        setStripeCustomerId(data.customer);
+      }
+      setEphemeralKey(data.ephemeralKey);
+      setPaymentIntent(data.paymentIntent);
+      setPublishableKey(data.publishableKey);
+  
+      console.log("Data at StripeServices:", data, " success: ", success);
+      const { error } = await initPaymentSheet({
+        merchantDisplayName: 'PropertyGo, Pte. Ltd.',
+        customerId: data.customer,
+        customerEphemeralKeySecret: data.ephemeralKey,
+        paymentIntentClientSecret: data.paymentIntent,
+        allowsDelayedPaymentMethods: true,
+        defaultBillingDetails: {
+          name: 'Jane Doe',
+        },
+      });
+  
+      if (!error) {
+        setLoading(true);
+      } else {
+        console.error('Error initializing payment sheet:', error);
+        throw error;
+      }
+    } catch (error) {
+      console.error('Error setting up payment sheet:', error);
+    }
+  };
+
+
+export const updateUserStripeCustomerId = async (stripeCustomerId, user, fetchUpdatedUserDetails) => {
+    const formData = new FormData();
+    console.log("stripeCustomerId: ", stripeCustomerId);
+    formData.append('stripeCustomerId', stripeCustomerId);
+    formData.append('email', user.email);
+    formData.append('countryOfOrigin', user.countryOfOrigin);
+    formData.append('dateOfBirth', user.dateOfBirth);
+
+    const { success, data, message } = await updateUserProfile(user.userId, formData);
+    console.log("success: ", success, " data: ", data, " message: ", message);
+    fetchUpdatedUserDetails();
+};
+
+export const createTransactionRecord = async (propertyListing, user, paymentIntent) => {
+    const { data, success, message } = await createTransaction({
+        onHoldBalance: propertyListing.optionFee,
+        buyerId: user.userId,
+        propertyId: propertyListing.propertyListingId,
+        stripePaymentResponse: paymentIntent,
+        invoiceId: 3,
+        status: "PENDING",
+        transactionType: "OPTION_FEE",
+    });
+    // console.log("createTransactionRecord - success: ", success, " data: ", data, " message: ", message);
+};
+
+export const fetchUpdatedUserDetails = async (user, login) => {
+    try {
+        const { success, data, message } = await loginUser(user.userName, user.password);
+
+        if (success) {
+            login(data);
+        } else {
+            Alert.alert('Error', message);
+        }
+    } catch (error) {
+        console.error('Error fetching updated user details:', error);
+    }
+};
+
+export const handleDeepLink = async (url, handleURLCallback) => {
+    if (url) {
+        const stripeHandled = await handleURLCallback(url);
+        if (stripeHandled) {
+            // This was a Stripe URL - you can return or add extra handling here as you see fit
+        } else {
+            // This was NOT a Stripe URL – handle as you normally would
+        }
+    }
+};
