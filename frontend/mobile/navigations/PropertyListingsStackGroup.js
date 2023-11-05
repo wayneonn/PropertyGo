fb802d7af22cf3fe52dc4478a3dbb2e52225fdc0
Wayne <e0726777@u.nus.edu>
2023-11-06 03:48:40 +0800

    Created seller onboarding

--- frontend/mobile/navigations/PropertyListingsStackGroup.js
+++ frontend/mobile/navigations/PropertyListingsStackGroup.js
@@ -1,3 +1,4 @@
+import React, {useContext} from 'react';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // import PropertyListing from "../screens/propertyListings/PropertyListings";
 import ListProperty from "../screens/userBottomNavBar/Sell";
@@ -11,28 +12,44 @@ import PurchaseOptionFee from "../screens/purchase/PurchaseOptionFeeCheckoutScre
 import PurchaseOptionFeeInfo from "../screens/purchase/PurchaseOptionFeeInfo";
 import Map from "../screens/propertyListings/map";
 import TokenScreen from "../screens/token/TokenScreen"; // Import your Coins screen here
-import TokenCheckoutScreen from "../screens/token/TokenCheckoutScreen"; 
+import TokenCheckoutScreen from "../screens/token/TokenCheckoutScreen";
+import SellerOnboarding from "../screens/propertyListings/SellerOnboarding";
+import { AuthContext } from '../AuthContext';
 
 const PropertyListingsStack = createNativeStackNavigator();
 
 const PropertyListingsStackGroup = () => {
+    const { user } = useContext(AuthContext);
+    const bankName = user.user.bankName;
+    const bankAccount = user.user.bankAccount;
+
+    const screens = bankName && bankAccount
+        ? [
+            
+            <PropertyListingsStack.Screen name="List Property" component={ListProperty} />,
+        ]
+        : [
+            <PropertyListingsStack.Screen name="Seller Onboarding" component={SellerOnboarding} />,
+            <PropertyListingsStack.Screen name="List Property" component={ListProperty} />
+        ];
 
     return (
-
-        <PropertyListingsStack.Navigator screenOptions={{headerShown: false}}>
+        <PropertyListingsStack.Navigator screenOptions={{ headerShown: false }}>
             {/* Adjust accordingly */}
             {/* <PropertyListingsStack.Screen name="Map" component={Map}/> */}
-            <PropertyListingsStack.Screen name="List Property" component={ListProperty}/>
-            <PropertyListingsStack.Screen name="Property Listing" component={PropertyListing}/>
-            <PropertyListingsStack.Screen name="Edit Property Listing" component={EditPropertyListing}/>
-            <PropertyListingsStack.Screen name="View Profile" component={ViewUserProfile}/>
-            <PropertyListingsStack.Screen name="Boost Listing" component={BoostPropertyListing}/>
-            <PropertyListingsStack.Screen name="Set Schedule" component={SetSchedule}/>
-            <PropertyListingsStack.Screen name="Schedule" component={Schedule}/>
-            <PropertyListingsStack.Screen name="Token" component={TokenScreen}/>
-            <PropertyListingsStack.Screen name="Token Checkout Screen" component={TokenCheckoutScreen}/>
-            <PropertyListingsStack.Screen name="Purchase Option Fee" component={PurchaseOptionFee}/>
-            <PropertyListingsStack.Screen name="Purchase Option Fee Info" component={PurchaseOptionFeeInfo}/>
+            {screens}
+            {/* <PropertyListingsStack.Screen name="List Property" component={ListProperty} /> */}
+            <PropertyListingsStack.Screen name="Property Listing" component={PropertyListing} />
+            <PropertyListingsStack.Screen name="Edit Property Listing" component={EditPropertyListing} />
+            <PropertyListingsStack.Screen name="View Profile" component={ViewUserProfile} />
+            <PropertyListingsStack.Screen name="Boost Listing" component={BoostPropertyListing} />
+            <PropertyListingsStack.Screen name="Set Schedule" component={SetSchedule} />
+            <PropertyListingsStack.Screen name="Schedule" component={Schedule} />
+            <PropertyListingsStack.Screen name="Token" component={TokenScreen} />
+            <PropertyListingsStack.Screen name="Token Checkout Screen" component={TokenCheckoutScreen} />
+            <PropertyListingsStack.Screen name="Purchase Option Fee" component={PurchaseOptionFee} />
+            <PropertyListingsStack.Screen name="Purchase Option Fee Info" component={PurchaseOptionFeeInfo} />
+            {/* <PropertyListingsStack.Screen name="Seller Onboarding" component={SellerOnboarding} /> */}
         </PropertyListingsStack.Navigator>
     );
 };
